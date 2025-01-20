22c9abe702b059e94fbd2d715d99ec4b15d52dd4
hippochld <19624046@mahasiswa.itb.ac.id>
2025-01-20 20:49:16 +0700

    Aksantara_2025
    Aksantara_2025

---
+++ Aksantara_2025/sketch_5/sketch_5.ino
@@ -0,0 +1,14 @@
+//Soal Firmware Aksantara 2025
+#define PWM_PIN 3
+#define PERIOD 2000
+
+void setup() {
+  pinMode(PWM_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(PWM_PIN, HIGH);
+  delayMicroseconds(PERIOD /2);
+  digitalWrite(PWM_PIN, LOW);
+  delayMicroseconds(PERIOD /2);
+}
